b962014f382ec271319bdcb9b81f93b6eed52af6
Andy Beable <andy.beable@gmail.com>
2025-07-28 11:02:51 +0100

    commits footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} Your Language Learning App</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
